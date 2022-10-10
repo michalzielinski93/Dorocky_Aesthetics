471d64029eb9fe8451e102e1349351543208b8a8
michalzielinski93 <michalzielinski930@gmail.com>
2022-10-10 20:02:33 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function redirectFunc () {
+    window.location.replace('/main_page/index.html')
+}
+setTimeout('redirectFunc()', 3000);
+
+// how to get previous page on refresh ----- important
+// window.location.reload('../loading_page/index.html')
+
+// function() {
+//     addEventListener.OnClick(re).replace('/main_page/index.html')
+// }
+// window.onclick.onbeforeunload(window.location.replace('/index.html'));
+
